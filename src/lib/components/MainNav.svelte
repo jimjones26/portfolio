6105dacf21a95c2fc7e4e2f9bf32c472cbf35a91
jjones <jimmy.jones@tutanota.com>
2023-09-30 12:23:50 -0600

    recator main nav href into main naiv item component

--- src/lib/components/MainNav.svelte
+++ src/lib/components/MainNav.svelte
@@ -1,5 +1,15 @@
+<script lang="ts">
+	import MainNavItem from './MainNavItem.svelte';
+
+	const mainNavItems = [
+		{ href: '/experience', label: 'Experience' },
+		{ href: '/portfolio', label: 'Portfolio' },
+		{ href: '/contact', label: 'Contact' }
+	];
+</script>
+
 <nav class="flex flex-col p-10">
-	<a href="/" class="mb-1 text-right w-fit self-end text-2xl font-normal mt-3">Experience</a>
-	<a href="/" class="mb-1 text-right w-fit self-end text-2xl font-normal mt-3">Portfolio</a>
-	<a href="/" class="mb-1 text-right w-fit self-end text-2xl font-normal mt-3">Contact</a>
+	{#each mainNavItems as navItem}
+		<MainNavItem {...navItem} />
+	{/each}
 </nav>
