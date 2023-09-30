27650705b16cab145313e80d0042da4de56873f5
jjones <jimmy.jones@tutanota.com>
2023-09-30 12:01:20 -0600

    establish main nav layout

--- src/lib/components/MainNav.svelte
+++ src/lib/components/MainNav.svelte
@@ -1,1 +1,5 @@
-main nav
+<nav class="flex flex-col p-10">
+	<a href="/" class="mb-1 text-right w-fit self-end text-2xl font-normal mt-3">Experience</a>
+	<a href="/" class="mb-1 text-right w-fit self-end text-2xl font-normal mt-3">Portfolio</a>
+	<a href="/" class="mb-1 text-right w-fit self-end text-2xl font-normal mt-3">Contact</a>
+</nav>
