01cf3d3718912bb121bd93db8adfe09698eacbcb
jjones <jimmy.jones@tutanota.com>
2023-09-30 22:15:32 -0600

    refactor main nav from layout to home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,9 @@
 <script>
-	import MainNav from '$lib/components/MainNav.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import '../app.postcss';
 </script>
 
 <div class="flex flex-col h-screen">
 	<Header />
-	<section class="flex-1 mx-10">
-		<slot />
-	</section>
-	<MainNav />
+	<slot />
 </div>
