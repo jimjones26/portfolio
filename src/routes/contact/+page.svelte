f9f85c3a7b986afa17a8ef22d67bab43675310e7
jjones <jimmy.jones@tutanota.com>
2023-10-01 17:21:29 -0600

    add contact page header and back button

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,17 @@
-contact
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { Button } from '$lib/components/ui/button';
+	import { ChevronLeft } from 'lucide-svelte';
+
+	function handleBack() {
+		goto('/');
+	}
+</script>
+
+<header class="px-4 py-9">
+	<nav class="flex items-center">
+		<Button variant="ghost" on:click={handleBack}
+			><ChevronLeft /><span class="font-sans text-base ml-4">Return Home</span></Button
+		>
+	</nav>
+</header>
