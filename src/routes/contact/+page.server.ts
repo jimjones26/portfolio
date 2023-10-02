a5191caf56711cd006965e929626231df839e08f
jjones <jimmy.jones@tutanota.com>
2023-10-01 20:14:36 -0600

    stub action to handle form submit

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,9 +1,26 @@
-import type { PageServerLoad } from "./$types";
+import type { PageServerLoad, Actions } from "./$types";
 import { superValidate } from "sveltekit-superforms/server";
 import { formSchema } from "./schema";
+import { fail } from "@sveltejs/kit";
 
 export const load: PageServerLoad = () => {
   return {
     form: superValidate(formSchema)
   };
+};
+
+export const actions: Actions = {
+  default: async (event) => {
+    const form = await superValidate(event, formSchema);
+    if (!form.valid) {
+      return fail(400, {
+        form
+      });
+    }
+    console.log("Form Data: ", form)
+    console.log("TODO: Add sendgrid support.")
+    return {
+      form
+    };
+  }
 };
