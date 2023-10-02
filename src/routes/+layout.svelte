78705544ed6729530177b0b689783823ab720958
jjones <jimmy.jones@tutanota.com>
2023-10-01 21:33:16 -0600

    add simple page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,18 @@
 <script>
+	import { fly } from 'svelte/transition';
 	import '../app.postcss';
+
+	export let data;
 </script>
 
-<div class="flex flex-col h-screen">
-	<slot />
-</div>
+<main class="h-screen">
+	{#key data.url}
+		<div
+			class="flex flex-col h-screen"
+			in:fly={{ x: -200, duration: 300, delay: 300 }}
+			out:fly={{ x: 200, duration: 300 }}
+		>
+			<slot />
+		</div>
+	{/key}
+</main>
